4d21dc50f95fdf3d2740f758eddae3f714d61ded
samuelrajasingh <rssambbb0123@gmail.com>
2020-01-12 19:16:05 +0530

    Second commit

--- PasswordGenerator.sh
+++ PasswordGenerator.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #Simple Password Generator 
-echo "This is a simple password generator"
-echo "Please enter the length of the password: "
-read PASS_LENGTH
+
+# Reg NO URK17CS290
+
+#I am using openssl to  generate random strings and encode it with  -base64 
+#If you dont provide length the default length will be 48
+# This is a simple password generator
+#You can set an alias of your choice for simplicity
+echo "Please enter the length of the password with the command"
+echo "Password length will be 48 by default"
+echo "you will get 5 different passwords"
+echo "---------------------------------"
 for p in $(seq 1 5);
 do
-	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+	openssl rand -base64 48 | cut -c1-$@
 done
+echo "---------------------------------"
